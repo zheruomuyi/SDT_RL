7b8f96f41ee6372b48ffb135d39d664447ce9cde
jinjin.liu <jinjin.liu@envision-digital.com>
2020-03-03 01:02:33 +0800

    rl_dppo

--- environment.py
+++ environment.py
@@ -1,22 +1,26 @@
-import numpy as np
-import pandas as pd
-import time
+import numpy
 
-from dateutil.parser import parse
-from datetime import datetime
-import http.client
+LAST_POINT_ADJUST_TIME = 30000
 
 
-class Env(object):
-    timestamp = 0
+class Adjust_env(object):
 
-    def __init__(self):
-        self.action_space = ['Enlarge', 'Unchanged', 'Reduce']
-        self.n_actions = len(self.action_space)
-        self.n_features = 2
-        self.title('maze')
-        # self.geometry('{0}x{1}'.format(MAZE_H * UNIT, MAZE_H * UNIT))
-        self._build_maze()
+    def __init__(self, comp_dev, comp_std, comp_proportion):
+        self.comp_dev = comp_dev
+        self.comp_std = comp_std
+        self.comp_proportion = comp_proportion
 
-    def _build_maze(self):
-        self.canvas = tk.Canvas(self)
+    def reset(self):
+        self.comp_dev = 0
+        self.comp_proportion = 0
+        self.comp_std = 0
+        return self
+
+    def step(self, action):
+        self.comp_dev = self.comp_dev + action["change"]
+        reward = action['comp_proportion'] / self.comp_proportion - action['comp_std'] / self.comp_std
+        done = 'false'
+        return self.comp_dev, reward, false,
+
+    def render(self):
+        return self.comp_dev, self.comp_proportion, self.comp_std
--- main.py
+++ main.py
@@ -1,94 +1,226 @@
-from environment import Env
-from RL_brain import DeepQNetwork
-import requests
-import json
-import pymysql as mysql
-import sys
+"""
+A simple version of OpenAI's Proximal Policy Optimization (PPO). [https://arxiv.org/abs/1707.06347]
+
+Distributing workers in parallel to collect data, then stop worker's roll-out and train PPO on collected data.
+Restart workers once PPO is updated.
+
+The global PPO updating rule is adopted from DeepMind's paper (DPPO):
+Emergence of Locomotion Behaviours in Rich Environments (Google Deepmind): [https://arxiv.org/abs/1707.02286]
+
+View more on my tutorial website: https://morvanzhou.github.io/tutorials
+
+Dependencies:
+python:3.5
+tensorflow r1.3
+"""
+
+import tensorflow as tf
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
+import threading
+import queue
+from environment import Adjust_env
+import redis
+import py_snappy
+import json
+from apscheduler.schedulers.blocking import BlockingScheduler
+import math
 
-url = "http://tsdb-queryengine.beta-k8s-cn4.eniot.io/dataService/assets/tsdb/rawData/timeRange"
-
-
-def run_main():
-    step = 0
-    for episode in range(300):
-        observation = Env.reset()
-        # DQN 根据观测值选择行为
-        action = RL.choose_action(observation)
-
-        # 环境根据行为给出下一个 state, reward, 是否终止
-        observation_, reward, done = env.step(action)
-
-        # DQN 存储记忆
-        RL.store_transition(observation, action, reward, observation_)
-
-        # 控制学习起始时间和频率 (先累积一些记忆再开始学习)
-        if (step > 200) and (step % 5 == 0):
-            RL.learn()
-
-        # 将下一个 state_ 变为 下次循环的 state
-        observation = observation_
-
-        # 如果终止, 就跳出循环
-        if done:
-            break
-        step += 1  # 总步数
-
-        # end of game
-    print('game over')
-    env.destroy()
+EP_MAX = 1000
+EP_LEN = 200
+N_WORKER = 4  # parallel workers
+GAMMA = 0.9  # reward discount factor
+A_LR = 0.0001  # learning rate for actor
+C_LR = 0.0002  # learning rate for critic
+MIN_BATCH_SIZE = 64  # minimum batch size for updating PPO
+UPDATE_STEP = 10  # loop update operation n-steps
+EPSILON = 0.2  # for clipping surrogate objective
+GAME = 'Pendulum-v0'
+S_DIM, A_DIM = 3, 1  # state and action dimension
+environments = {}
 
 
-if __name__ == "__main__":
-    conn = mysql.connect(host="172.20.16.64", port=3306, user="root", password="root", database="compress_policy",
-                         charset="utf8")
-    try:
-        with conn.cursor() as cursor:
-            sql = "select * from model_point_storage_policy_info where `point_compress` = 1"
-            cursor.execute(sql)
-            result = cursor.fetchall()
-    finally:
-        cursor.close()
-        conn.close()
+class PPO(object):
+    def __init__(self):
+        self.sess = tf.Session()
+        self.tfs = tf.placeholder(tf.float32, [None, S_DIM], 'state')
 
-    # print(result)
-    for item in result:
-        # print(item)
-        point_id = item[0]
-        model_id = item[1]
-        org_id = item[2]
-        storage_policy_id = item[3]
-        point_com_dev = json.loads(item[5])
-        asset_ids = point_com_dev['assetIds']
-        comp_dev = point_com_dev['compDev']
-        print(org_id, '\t', model_id, '\t', asset_ids, '\t', storage_policy_id, '\t', comp_dev, '\t', point_id)
+        # critic
+        l1 = tf.layers.dense(self.tfs, 100, tf.nn.relu)
+        self.v = tf.layers.dense(l1, 1)
+        self.tfdc_r = tf.placeholder(tf.float32, [None, 1], 'discounted_r')
+        self.advantage = self.tfdc_r - self.v
+        self.closs = tf.reduce_mean(tf.square(self.advantage))
+        self.ctrain_op = tf.train.AdamOptimizer(C_LR).minimize(self.closs)
 
-        querystring = {"accessKey": "EEOP_TEST", "orgId": org_id, "assetIds": 'Qaae2jze',
-                       "measurepoints": point_id, 'pageSize': 640000,
-                       "startTime": "2020-01-05 00:00:00", "endTime": "2020-01-05 23:59:59",
-                       "localTimeAccuracy": "False"}
-        response = requests.request("GET", url, params=querystring)
-        asset_ids = ['Qaae2jze']
-        data = json.loads(response.text)['data']['items']
-        ts = pd.DataFrame(data)
-        # ts['timestamp'] = pd.to_datetime(ts['timestamp'])
-        print(len(ts))
-        # for value_item in ts:
-        #     timestamp = value_item['timestamp']
-        #     value = 'null'
-        #     if point_id in value_item:
-        #         value = value_item[point_id]
-        #     print(timestamp, '\t', value)
-        env = Env(ts['timestamp',point_id])
-    RL = DeepQNetwork(env.n_actions, env.n_features,
-                      learning_rate=0.01,
-                      reward_decay=0.9,
-                      e_greedy=0.9,
-                      replace_target_iter=200,  # 每 200 步替换一次 target_net 的参数
-                      memory_size=2000,  # 记忆上限
-                      # output_graph=True   # 是否输出 tensorboard 文件
-                      )
-    env.after(100, run_main)
-    env.mainloop()
-    RL.plot_cost()  # 观看神经网络的误差曲线
+        # actor
+        pi, pi_params = self._build_anet('pi', trainable=True)
+        oldpi, oldpi_params = self._build_anet('oldpi', trainable=False)
+        self.sample_op = tf.squeeze(pi.sample(1), axis=0)  # operation of choosing action
+        self.update_oldpi_op = [oldp.assign(p) for p, oldp in zip(pi_params, oldpi_params)]
+
+        self.tfa = tf.placeholder(tf.float32, [None, A_DIM], 'action')
+        self.tfadv = tf.placeholder(tf.float32, [None, 1], 'advantage')
+        # ratio = tf.exp(pi.log_prob(self.tfa) - oldpi.log_prob(self.tfa))
+        ratio = pi.prob(self.tfa) / (oldpi.prob(self.tfa) + 1e-5)
+        surr = ratio * self.tfadv  # surrogate loss
+
+        self.aloss = -tf.reduce_mean(tf.minimum(  # clipped surrogate objective
+            surr,
+            tf.clip_by_value(ratio, 1. - EPSILON, 1. + EPSILON) * self.tfadv))
+
+        self.atrain_op = tf.train.AdamOptimizer(A_LR).minimize(self.aloss)
+        self.sess.run(tf.global_variables_initializer())
+
+    def update(self):
+        global GLOBAL_UPDATE_COUNTER
+        while not COORD.should_stop():
+            if GLOBAL_EP < EP_MAX:
+                UPDATE_EVENT.wait()  # wait until get batch of data
+                self.sess.run(self.update_oldpi_op)  # copy pi to old pi
+                data = [QUEUE.get() for _ in range(QUEUE.qsize())]  # collect data from all workers
+                data = np.vstack(data)
+                s, a, r = data[:, :S_DIM], data[:, S_DIM: S_DIM + A_DIM], data[:, -1:]
+                adv = self.sess.run(self.advantage, {self.tfs: s, self.tfdc_r: r})
+                # update actor and critic in a update loop
+                [self.sess.run(self.atrain_op, {self.tfs: s, self.tfa: a, self.tfadv: adv}) for _ in range(UPDATE_STEP)]
+                [self.sess.run(self.ctrain_op, {self.tfs: s, self.tfdc_r: r}) for _ in range(UPDATE_STEP)]
+                UPDATE_EVENT.clear()  # updating finished
+                GLOBAL_UPDATE_COUNTER = 0  # reset counter
+                ROLLING_EVENT.set()  # set roll-out available
+
+    def _build_anet(self, name, trainable):
+        with tf.variable_scope(name):
+            l1 = tf.layers.dense(self.tfs, 200, tf.nn.relu, trainable=trainable)
+            mu = 2 * tf.layers.dense(l1, A_DIM, tf.nn.tanh, trainable=trainable)
+            sigma = tf.layers.dense(l1, A_DIM, tf.nn.softplus, trainable=trainable)
+            norm_dist = tf.distributions.Normal(loc=mu, scale=sigma)
+        params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=name)
+        return norm_dist, params
+
+    def choose_action(self, s):
+        s = s[np.newaxis, :]
+        a = self.sess.run(self.sample_op, {self.tfs: s})[0]
+        return np.clip(a, -2, 2)
+
+    def get_v(self, s):
+        if s.ndim < 2: s = s[np.newaxis, :]
+        return self.sess.run(self.v, {self.tfs: s})[0, 0]
+
+
+class Worker(object):
+    def __init__(self, wid):
+        self.wid = wid
+        # self.env = gym.make(GAME).unwrapped
+        # self.environment = env
+        self.ppo = GLOBAL_PPO
+
+    def work(self):
+        global GLOBAL_EP, GLOBAL_RUNNING_R, GLOBAL_UPDATE_COUNTER
+
+        while not COORD.should_stop():
+            for environment in environments:
+                s = environment.reset()
+                ep_r = 0
+                buffer_s, buffer_a, buffer_r = [], [], []
+                for t in range(EP_LEN):
+                    if not ROLLING_EVENT.is_set():  # while global PPO is updating
+                        ROLLING_EVENT.wait()  # wait until PPO is updated
+                        buffer_s, buffer_a, buffer_r = [], [], []  # clear history buffer, use new policy to collect data
+                    a = self.ppo.choose_action(s)
+                    s_, r, done, _ = environment.step(a)
+                    buffer_s.append(s)
+                    buffer_a.append(a)
+                    buffer_r.append((r + 8) / 8)  # normalize reward, find to be useful
+                    s = s_
+                    ep_r += r
+
+                    GLOBAL_UPDATE_COUNTER += 1  # count to minimum batch size, no need to wait other workers
+                    if t == EP_LEN - 1 or GLOBAL_UPDATE_COUNTER >= MIN_BATCH_SIZE:
+                        v_s_ = self.ppo.get_v(s_)
+                        discounted_r = []  # compute discounted reward
+                        for r in buffer_r[::-1]:
+                            v_s_ = r + GAMMA * v_s_
+                            discounted_r.append(v_s_)
+                        discounted_r.reverse()
+
+                        bs, ba, br = np.vstack(buffer_s), np.vstack(buffer_a), np.array(discounted_r)[:, np.newaxis]
+                        buffer_s, buffer_a, buffer_r = [], [], []
+                        QUEUE.put(np.hstack((bs, ba, br)))  # put data in the queue
+                        if GLOBAL_UPDATE_COUNTER >= MIN_BATCH_SIZE:
+                            ROLLING_EVENT.clear()  # stop collecting data
+                            UPDATE_EVENT.set()  # globalPPO update
+
+                        if GLOBAL_EP >= EP_MAX:  # stop training
+                            COORD.request_stop()
+                            break
+
+                # record reward changes, plot later
+                if len(GLOBAL_RUNNING_R) == 0:
+                    GLOBAL_RUNNING_R.append(ep_r)
+                else:
+                    GLOBAL_RUNNING_R.append(GLOBAL_RUNNING_R[-1] * 0.9 + ep_r * 0.1)
+                GLOBAL_EP += 1
+                print('{0:.1f}%'.format(GLOBAL_EP / EP_MAX * 100), '|W%i' % self.wid, '|Ep_r: %.2f' % ep_r, )
+
+
+def adjustJob():
+    pool = redis.ConnectionPool(host='localhost', port=6379)
+    r = redis.Redis(connection_pool=pool)
+    keys = r.keys()
+    # print(keys)
+
+    for key in keys:
+        last_points = r.hgetall(key)
+        # print(lastPoints)
+        for asset_point in last_points:
+            last_point = bytes.decode(py_snappy.decompress(last_points[asset_point]))
+            last_point_json = json.loads(last_point)
+            comp_error = last_point_json['compError'].double()
+            comp_dev = last_point_json['compDev'].double()
+            before_comp = last_point_json['beforeComp']
+            after_comp = last_point_json['afterComp']
+            comp_std = math.sqrt(comp_error / before_comp)
+            comp_proportion = before_comp.long() / after_comp.long()
+            if asset_point in environments.keys():
+                env = environments[asset_point]
+                s = env.render()
+                change = GLOBAL_PPO.choose_action(s)
+                action = {'change': change, 'comp_proportion': comp_proportion, 'comp_std': comp_std}
+                env.step(action)
+            else:
+                env = Adjust_env(comp_dev, comp_std, comp_proportion)
+                environments[asset_point] = env
+
+
+if __name__ == '__main__':
+    GLOBAL_PPO = PPO()
+    UPDATE_EVENT, ROLLING_EVENT = threading.Event(), threading.Event()
+    UPDATE_EVENT.clear()  # not update now
+    ROLLING_EVENT.set()  # start to roll out
+    workers = [Worker(wid=i) for i in range(N_WORKER)]
+
+    GLOBAL_UPDATE_COUNTER, GLOBAL_EP = 0, 0
+    GLOBAL_RUNNING_R = []
+    COORD = tf.train.Coordinator()
+    QUEUE = queue.Queue()  # workers putting data in this queue
+    threads = []
+    for worker in workers:  # worker threads
+        t = threading.Thread(target=worker.work, args=())
+        t.start()  # training
+        threads.append(t)
+    # add a PPO updating thread
+    threads.append(threading.Thread(target=GLOBAL_PPO.update, ))
+    threads[-1].start()
+    COORD.join(threads)
+
+    # plot reward change and test
+    plt.plot(np.arange(len(GLOBAL_RUNNING_R)), GLOBAL_RUNNING_R)
+    plt.xlabel('Episode')
+    plt.ylabel('Moving reward')
+    plt.ion()
+    plt.show()
+
+    # env = Adjust_env()
+    scheduler = BlockingScheduler()
+    scheduler.add_job(adjustJob, 'interval', seconds=30)
