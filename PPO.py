4a0fc6f6e754a0c32649be99090cf7b9d8847109
jinjin.liu <jinjin.liu@envision-digital.com>
2020-03-06 11:16:32 +0800

    dpo

--- PPO.py
+++ PPO.py
@@ -1,14 +1,4 @@
 """
-A simple version of OpenAI's Proximal Policy Optimization (PPO). [https://arxiv.org/abs/1707.06347]
-
-Distributing workers in parallel to collect data, then stop worker's roll-out and train PPO on collected data.
-Restart workers once PPO is updated.
-
-The global PPO updating rule is adopted from DeepMind's paper (DPPO):
-Emergence of Locomotion Behaviours in Rich Environments (Google Deepmind): [https://arxiv.org/abs/1707.02286]
-
-View more on my tutorial website: https://morvanzhou.github.io/tutorials
-
 Dependencies:
 python:3.5
 tensorflow r1.3
@@ -25,6 +15,33 @@ import py_snappy
 import json
 from apscheduler.schedulers.blocking import BlockingScheduler
 import math
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(__name__)
+redis_json = {}
+env = Adjust_env()
+
+
+@app.route('/adjust', methods=['POST'])
+def adjust():
+    if request.method == 'POST':
+        global redis_json
+        redis_json = request.json
+        print(redis_json)
+        scheduler = BlockingScheduler()
+        scheduler.add_job(adjust_job, 'interval', seconds=300)
+        scheduler.start()
+        return jsonify({'status': 0, "msg": 'start rl adjust'})
+    else:
+        return jsonify({'status': 1, "msg": 'start rl adjust wrong'})
+
+
+@app.route('/adjust', methods=['GET'])
+def adjust_get():
+    return jsonify({'status': 1, "msg": 'start rl adjust wrong'})
+
 
 EP_MAX = 1000
 EP_LEN = 200
@@ -37,7 +54,7 @@ UPDATE_STEP = 10  # loop update operation n-steps
 EPSILON = 0.2  # for clipping surrogate objective
 GAME = 'Pendulum-v0'
 S_DIM, A_DIM = 3, 1  # state and action dimension
-environments = {}
+LAST_POINT_ADJUST_TIME = 30000
 
 
 class PPO(object):
@@ -99,9 +116,10 @@ class PPO(object):
         return norm_dist, params
 
     def choose_action(self, s):
+        step_len = s[0]
         s = s[np.newaxis, :]
         a = self.sess.run(self.sample_op, {self.tfs: s})[0]
-        return np.clip(a, -2, 2)
+        return np.clip(a, -step_len, step_len)
 
     def get_v(self, s):
         if s.ndim < 2: s = s[np.newaxis, :]
@@ -111,86 +129,98 @@ class PPO(object):
 class Worker(object):
     def __init__(self, wid):
         self.wid = wid
-        # self.env = gym.make(GAME).unwrapped
-        # self.environment = env
+        self.environment = Adjust_env()
         self.ppo = GLOBAL_PPO
 
     def work(self):
         global GLOBAL_EP, GLOBAL_RUNNING_R, GLOBAL_UPDATE_COUNTER
 
         while not COORD.should_stop():
-            for environment in environments:
-                s = environment.reset()
-                ep_r = 0
-                buffer_s, buffer_a, buffer_r = [], [], []
-                for t in range(EP_LEN):
-                    if not ROLLING_EVENT.is_set():  # while global PPO is updating
-                        ROLLING_EVENT.wait()  # wait until PPO is updated
-                        buffer_s, buffer_a, buffer_r = [], [], []  # clear history buffer, use new policy to collect data
-                    a = self.ppo.choose_action(s)
-                    s_, r, done, _ = environment.step(a)
-                    buffer_s.append(s)
-                    buffer_a.append(a)
-                    buffer_r.append((r + 8) / 8)  # normalize reward, find to be useful
-                    s = s_
-                    ep_r += r
+            s = self.environment.reset()
+            ep_r = 0
+            buffer_s, buffer_a, buffer_r = [], [], []
+            for t in range(EP_LEN):
+                if not ROLLING_EVENT.is_set():  # while global PPO is updating
+                    ROLLING_EVENT.wait()  # wait until PPO is updated
+                    buffer_s, buffer_a, buffer_r = [], [], []
+                    # clear history buffer, use new policy to collect data
+                a = self.ppo.choose_action(s)
+                s_, r, done, _ = self.environment.step(a)
+                buffer_s.append(s)
+                buffer_a.append(a)
+                buffer_r.append((r + 8) / 8)  # normalize reward, find to be useful
+                s = s_
+                ep_r += r
 
-                    GLOBAL_UPDATE_COUNTER += 1  # count to minimum batch size, no need to wait other workers
-                    if t == EP_LEN - 1 or GLOBAL_UPDATE_COUNTER >= MIN_BATCH_SIZE:
-                        v_s_ = self.ppo.get_v(s_)
-                        discounted_r = []  # compute discounted reward
-                        for r in buffer_r[::-1]:
-                            v_s_ = r + GAMMA * v_s_
-                            discounted_r.append(v_s_)
-                        discounted_r.reverse()
+                GLOBAL_UPDATE_COUNTER += 1  # count to minimum batch size, no need to wait other workers
+                if t == EP_LEN - 1 or GLOBAL_UPDATE_COUNTER >= MIN_BATCH_SIZE:
+                    v_s_ = self.ppo.get_v(s_)
+                    discounted_r = []  # compute discounted reward
+                    for r in buffer_r[::-1]:
+                        v_s_ = r + GAMMA * v_s_
+                        discounted_r.append(v_s_)
+                    discounted_r.reverse()
 
-                        bs, ba, br = np.vstack(buffer_s), np.vstack(buffer_a), np.array(discounted_r)[:, np.newaxis]
-                        buffer_s, buffer_a, buffer_r = [], [], []
-                        QUEUE.put(np.hstack((bs, ba, br)))  # put data in the queue
-                        if GLOBAL_UPDATE_COUNTER >= MIN_BATCH_SIZE:
-                            ROLLING_EVENT.clear()  # stop collecting data
-                            UPDATE_EVENT.set()  # globalPPO update
+                    bs, ba, br = np.vstack(buffer_s), np.vstack(buffer_a), np.array(discounted_r)[:, np.newaxis]
+                    buffer_s, buffer_a, buffer_r = [], [], []
+                    QUEUE.put(np.hstack((bs, ba, br)))  # put data in the queue
+                    if GLOBAL_UPDATE_COUNTER >= MIN_BATCH_SIZE:
+                        ROLLING_EVENT.clear()  # stop collecting data
+                        UPDATE_EVENT.set()  # globalPPO update
 
-                        if GLOBAL_EP >= EP_MAX:  # stop training
-                            COORD.request_stop()
-                            break
+                    if GLOBAL_EP >= EP_MAX:  # stop training
+                        COORD.request_stop()
+                        break
 
-                # record reward changes, plot later
-                if len(GLOBAL_RUNNING_R) == 0:
-                    GLOBAL_RUNNING_R.append(ep_r)
-                else:
-                    GLOBAL_RUNNING_R.append(GLOBAL_RUNNING_R[-1] * 0.9 + ep_r * 0.1)
-                GLOBAL_EP += 1
-                print('{0:.1f}%'.format(GLOBAL_EP / EP_MAX * 100), '|W%i' % self.wid, '|Ep_r: %.2f' % ep_r, )
+            # record reward changes, plot later
+            if len(GLOBAL_RUNNING_R) == 0:
+                GLOBAL_RUNNING_R.append(ep_r)
+            else:
+                GLOBAL_RUNNING_R.append(GLOBAL_RUNNING_R[-1] * 0.9 + ep_r * 0.1)
+            GLOBAL_EP += 1
+            print('{0:.1f}%'.format(GLOBAL_EP / EP_MAX * 100), '|W%i' % self.wid, '|Ep_r: %.2f' % ep_r, )
 
 
-def adjustJob():
-    pool = redis.ConnectionPool(host='localhost', port=6379)
+def adjust_job():
+    pool = redis.ConnectionPool(host=redis_json['host'], port=redis_json['port'], password=redis_json['password'])
     r = redis.Redis(connection_pool=pool)
+    # r = redis.Redis(host=redis_json['host'], port=redis_json['port'])
     keys = r.keys()
-    # print(keys)
+    print(keys)
 
     for key in keys:
         last_points = r.hgetall(key)
+        last_points_update = {}
         # print(lastPoints)
         for asset_point in last_points:
+            print(asset_point)
             last_point = bytes.decode(py_snappy.decompress(last_points[asset_point]))
             last_point_json = json.loads(last_point)
-            comp_error = last_point_json['compError'].double()
-            comp_dev = last_point_json['compDev'].double()
-            before_comp = last_point_json['beforeComp']
-            after_comp = last_point_json['afterComp']
-            comp_std = math.sqrt(comp_error / before_comp)
-            comp_proportion = before_comp.long() / after_comp.long()
-            if asset_point in environments.keys():
-                env = environments[asset_point]
-                s = env.render()
+            next_time = last_point_json['nextTime']
+            point_time = last_point_json['lastPoint']['ts']
+            if next_time <= point_time:
+                comp_frequency = last_point_json['compFrequency']
+                comp_error = last_point_json['compError']
+                before_comp = last_point_json['beforeComp']
+                after_comp = last_point_json['afterComp']
+                comp_std = math.sqrt(comp_error / before_comp)
+                comp_proportion = before_comp / after_comp
+                s = env.render()[0]
                 change = GLOBAL_PPO.choose_action(s)
                 action = {'change': change, 'comp_proportion': comp_proportion, 'comp_std': comp_std}
-                env.step(action)
-            else:
-                env = Adjust_env(comp_dev, comp_std, comp_proportion)
-                environments[asset_point] = env
+                comp_dev = env.step(action)[0]
+                last_point_json['compDev'] = comp_dev
+                last_point_json['nextTime'] = next_time + (comp_frequency % 60) * LAST_POINT_ADJUST_TIME
+                last_point_json['beforeSum'] = last_point_json['beforeSum'] + before_comp
+                last_point_json['afterSum'] = last_point_json['afterSum'] + after_comp
+                last_point_json['beforeComp'] = 0
+                last_point_json['afterComp'] = 0
+                last_point_json['compStd'] = last_point_json['compStd'] + comp_std
+                last_point_json['compError'] = 0
+                last_point_json['compFrequency'] = comp_frequency + 1
+                last_points_update[asset_point] = last_point_json
+        if len(last_points_update) > 0:
+            r.hmset(key, last_points_update)
 
 
 if __name__ == '__main__':
@@ -220,7 +250,7 @@ if __name__ == '__main__':
     plt.ylabel('Moving reward')
     plt.ion()
     plt.show()
+    app.run(host='0.0.0.0', port=8080, debug=True)
+    adjust_get.run(host='0.0.0.0', port=8080, debug=True)
 
     # env = Adjust_env()
-    scheduler = BlockingScheduler()
-    scheduler.add_job(adjustJob, 'interval', seconds=30)
--- environment.py
+++ environment.py
@@ -1,26 +1,31 @@
-import numpy
+import numpy as np
 
 LAST_POINT_ADJUST_TIME = 30000
 
 
 class Adjust_env(object):
 
-    def __init__(self, comp_dev, comp_std, comp_proportion):
-        self.comp_dev = comp_dev
-        self.comp_std = comp_std
-        self.comp_proportion = comp_proportion
+    def __init__(self):
+        self.comp_dev = 0
+        self.comp_std = 0
+        self.comp_proportion = 0
+        self.s = np.array([self.comp_std, self.comp_dev])
 
     def reset(self):
         self.comp_dev = 0
         self.comp_proportion = 0
         self.comp_std = 0
-        return self
+        self.s = np.array([self.comp_std, self.comp_dev])
+        return self.s
 
     def step(self, action):
         self.comp_dev = self.comp_dev + action["change"]
         reward = action['comp_proportion'] / self.comp_proportion - action['comp_std'] / self.comp_std
-        done = 'false'
-        return self.comp_dev, reward, false,
+        self.comp_std = action['comp_std']
+        self.comp_proportion = action['comp_proportion']
+        self.s = {'comp_dev': self.comp_dev, 'comp_std': self.comp_std}
+        done = 0
+        return self.comp_dev, reward, done
 
     def render(self):
-        return self.comp_dev, self.comp_proportion, self.comp_std
+        return self.s, self.comp_std
